f8e6110aa3d467983d08fc0f46589222e6112d80
divizia21 <divizia21@bk.ru>
2022-09-29 19:20:12 +0300

    'client:react\redux__server:nodejs\express\mongoDB\JWT'

---
+++ Server/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const mongoose = require('mongoose')
+
+const PORT = process.env.SERVER_PORT || 5000
+const DB_URL = process.env.DB_URL
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+
+const start = async () => {
+    try{
+        await mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => {
+            console.log(`Server has been started on port: ${PORT}`)
+            console.log('MongoDB connected')
+        })
+
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+start()
